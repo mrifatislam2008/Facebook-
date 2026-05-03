4cab6d63bdc38f610d219cd2ee720d7c3e15c702
Md Rifat islam <mrifatislam2008@gmail.com>
2026-05-03 16:46:30 +0600

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,13 +1,21 @@
-import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
-const PASSWORD = process.env.ADMIN_PASSWORD;
-
 export default async function handler(req, res) {
-  const { password } = req.body;
+  let body = req.body;
 
-  if (password === PASSWORD) {
-    const token = jwt.sign({ admin: true }, process.env.JWT_SECRET, { expiresIn: "1d" });
+  if (typeof body === "string") {
+    body = JSON.parse(body);
+  }
+
+  const { password } = body;
+
+  if (password === process.env.ADMIN_PASSWORD) {
+    const token = jwt.sign(
+      { admin: true },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
     return res.json({ success: true, token });
   }
 
