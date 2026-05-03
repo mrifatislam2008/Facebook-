4508915b10f9a212cf75a32d3106cc6da5e767dd
Md Rifat islam <mrifatislam2008@gmail.com>
2026-05-03 16:47:24 +0600

    Add authorization check to create handler

--- api/create.js
+++ api/create.js
@@ -1,6 +1,12 @@
+import { verify } from "./middleware.js";
+
 let sites = {};
 
 export default function handler(req, res) {
+  if (!verify(req)) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
   const { name, code } = req.body;
 
   sites[name] = {
